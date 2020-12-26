3f2748a069118b8eaee6b63f60cff32d73e43b90
ozang <ozan@pixerylabs.com>
2020-12-26 21:58:47 +0300

    change covidnet to large

--- cnn/models/covidnet.py
+++ cnn/models/covidnet.py
@@ -75,7 +75,17 @@ class CovidNet(nn.Module):
             else:
                 self.add_module(key, PEXP(filters[key][0], filters[key][1]))
 
-        self.__forward__ = self.forward_small_net
+        # large net
+        self.add_module('conv1_1x1', nn.Conv2d(in_channels=64, out_channels=256, kernel_size=1))
+        self.add_module('conv2_1x1', nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1))
+        self.add_module('conv3_1x1', nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=1))
+        self.add_module('conv4_1x1', nn.Conv2d(in_channels=1024, out_channels=2048, kernel_size=1))
+
+        self.__forward__ = self.forward_large_net
+
+        # small net
+        # self.__forward__ = self.forward_small_net
+
         self.add_module('flatten', nn.Flatten())
         self.add_module('fc1', nn.Linear(7 * 7 * 2048, 1024))
 
@@ -112,6 +122,51 @@ class CovidNet(nn.Module):
         pepx42 = self.pexp4_2(pepx41)
         pepx43 = self.pexp4_3(pepx41 + pepx42)
         flattened = self.flatten(pepx41 + pepx42 + pepx43)
+
+        fc1out = F.relu(self.fc1(flattened))
+        fc2out = F.relu(self.fc2(fc1out))
+        logits = self.classifier(fc2out)
+        return logits
+
+    def forward_large_net(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        out_conv1_1x1 = self.conv1_1x1(x)
+
+        pepx11 = self.pexp1_1(x)
+        pepx12 = self.pexp1_2(pepx11 + out_conv1_1x1)
+        pepx13 = self.pexp1_3(pepx12 + pepx11 + out_conv1_1x1)
+
+        out_conv2_1x1 = F.max_pool2d(self.conv2_1x1(pepx12 + pepx11 + pepx13 + out_conv1_1x1), 2)
+
+        pepx21 = self.pexp2_1(
+            F.max_pool2d(pepx13, 2) + F.max_pool2d(pepx11, 2) + F.max_pool2d(pepx12, 2) + F.max_pool2d(out_conv1_1x1,
+                                                                                                       2))
+        pepx22 = self.pexp2_2(pepx21 + out_conv2_1x1)
+        pepx23 = self.pexp2_3(pepx22 + pepx21 + out_conv2_1x1)
+        pepx24 = self.pexp2_4(pepx23 + pepx21 + pepx22 + out_conv2_1x1)
+
+        out_conv3_1x1 = F.max_pool2d(self.conv3_1x1(pepx22 + pepx21 + pepx23 + pepx24 + out_conv2_1x1), 2)
+
+        pepx31 = self.pexp3_1(
+            F.max_pool2d(pepx24, 2) + F.max_pool2d(pepx21, 2) + F.max_pool2d(pepx22, 2) + F.max_pool2d(pepx23,
+                                                                                                       2) + F.max_pool2d(
+                out_conv2_1x1, 2))
+        pepx32 = self.pexp3_2(pepx31 + out_conv3_1x1)
+        pepx33 = self.pexp3_3(pepx31 + pepx32 + out_conv3_1x1)
+        pepx34 = self.pexp3_4(pepx31 + pepx32 + pepx33 + out_conv3_1x1)
+        pepx35 = self.pexp3_5(pepx31 + pepx32 + pepx33 + pepx34 + out_conv3_1x1)
+        pepx36 = self.pexp3_6(pepx31 + pepx32 + pepx33 + pepx34 + pepx35 + out_conv3_1x1)
+
+        out_conv4_1x1 = F.max_pool2d(
+            self.conv4_1x1(pepx31 + pepx32 + pepx33 + pepx34 + pepx35 + pepx36 + out_conv3_1x1), 2)
+
+        pepx41 = self.pexp4_1(
+            F.max_pool2d(pepx31, 2) + F.max_pool2d(pepx32, 2) + F.max_pool2d(pepx32, 2) + F.max_pool2d(pepx34,
+                                                                                                       2) + F.max_pool2d(
+                pepx35, 2) + F.max_pool2d(pepx36, 2) + F.max_pool2d(out_conv3_1x1, 2))
+        pepx42 = self.pexp4_2(pepx41 + out_conv4_1x1)
+        pepx43 = self.pexp4_3(pepx41 + pepx42 + out_conv4_1x1)
+        flattened = self.flatten(pepx41 + pepx42 + pepx43 + out_conv4_1x1)
 
         fc1out = F.relu(self.fc1(flattened))
         fc2out = F.relu(self.fc2(fc1out))
