efebaa427fe4a0254bc342854b25d15f2a740cb2
ozang <ozan@pixerylabs.com>
2021-01-01 21:21:08 +0300

    logs

--- ml/helper.py
+++ ml/helper.py
@@ -26,21 +26,23 @@ def get_prediction_kf(kf, model, X, y, tag=None):
     for e, (train, test) in enumerate(tqdm(kf.split(X, y))):
         if not isinstance(X, np.ndarray):
             X = np.array(X)
-
+        # log.info("iteration - {}".format(e))
         X_train, X_test, y_train, y_test = X[train], X[test], np.array(y)[train], np.array(y)[test]
-
+        # log.info("prepared data")
         model.fit(X_train, y_train)
+        # log.info("model has been fitted")
         success_ratio = model.score(X_test, y_test)
-        # log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " Test Success Ratio: " + str(100*success_ratio) + "%")
+        # log.info("got the score")
+        log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " Test Success Ratio: " + str(100*success_ratio) + "%")
         ratios.append(success_ratio)
 
         test_prob = model._predict_proba_lr(X_test) if isinstance(model, LinearSVC) else model.predict_proba(X_test)
         auc = roc_auc_score(y_test, test_prob, multi_class="ovr")
-        # log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " AUC Score: " + str(auc))
+        log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " AUC Score: " + str(auc))
         roc_list.append(auc)
 
         conf_matrix = confusion_matrix(y_test.tolist(), model.predict(X_test).tolist())
-        # log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " Confusion Matrix:\n" + str(conf_matrix))
+        log.info(str(cv) + "-Fold CV -- Iteration " + str(e) + " Confusion Matrix:\n" + str(conf_matrix))
         conf_matrices.append(conf_matrix)
 
         if tag is not None:
